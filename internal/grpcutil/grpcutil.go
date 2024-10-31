2f0925ed91971f0d7a9419ded378f978eaf23f22
nelia247 <cornelia@cloudplexo.com>
2024-10-31 02:16:04 +0100

    my commit

--- internal/grpcutil/grpcutil.go
+++ internal/grpcutil/grpcutil.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-// ServiceConnection attempts to select a random service instance and returns a gRPC connection to it.
+// ServiceConnection attempts to select a random service instance and returns a gRPC connection to it this is just a test to see if i show up.
 func ServiceConnection(ctx context.Context, serviceName string, registry discovery.Registry) (*grpc.ClientConn, error) {
 	addrs, err := registry.ServiceAddresses(ctx, serviceName)
 	if err != nil {
